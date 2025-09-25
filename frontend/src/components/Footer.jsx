e4d44e50bfb0f3e8ee24eda8c41ad87cec72bf6f
Say-Selenya <icadellans20@gmail.com>
2025-09-25 16:10:16 +0200

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 text-center py-6 mt-10 border-t border-gray-800">
+      <p>©️ {new Date().getFullYear()} Zäe Selenya Portal 🌙 — Todos los universos creativos reservados</p>
+    </footer>
+  );
+}
