98bb22a37457f1fd1d237169ffb54a2f775d514f
Say-Selenya <icadellans20@gmail.com>
2025-09-26 02:24:34 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
 import Hero from "./components/Hero";
-import Intro from "./components/Intro";
-import Tiers from "./components/Tiers";
-import Ofrendas from "./components/Ofrendas";
-import Contacto from "./components/Contacto";
-import Footer from "./components/Footer";
-import "./index.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="bg-black text-white min-h-screen font-sans">
+    <div>
       <Hero />
-      <main className="max-w-6xl mx-auto px-4 py-10 space-y-12">
-        <Intro />
-        <Tiers />
-        <Ofrendas />
-        <Contacto />
-      </main>
-      <Footer />
     </div>
   );
 }
+
+export default App;
