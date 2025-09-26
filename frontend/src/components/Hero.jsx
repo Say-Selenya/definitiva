c01f53aab23ed3d71b8e3602a8867d1ce8f8c4ba
Say-Selenya <icadellans20@gmail.com>
2025-09-26 02:27:11 +0200

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section
       className="relative w-full h-[70vh] flex items-center justify-center text-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/imagenes/sombra.jpg')" }}
+style={{ backgroundImage: "url('/imagenes/sombra.jpg')" }}
     >
       {/* Capa oscura encima de la foto */}
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
