c6643c7fb70d657bff657f1611aba9e799e45a06
Say-Selenya <icadellans20@gmail.com>
2025-09-28 01:30:04 +0200

    Update Zaelanding.jsx

--- frontend/src/components/Zaelanding.jsx
+++ frontend/src/components/Zaelanding.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import ZaeLanding from "./components/ZaeLanding";
 
+function App() {
+  return (
+    <div className="App">
+      <ZaeLanding />
+    </div>
+  );
+}
+
+export default App;
